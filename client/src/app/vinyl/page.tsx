d84065265bfcc4dc1073779a6eeddf81b4fbf785
Nxtiphong <naynaynay2233@gmail.com>
2025-02-09 22:29:55 +0700

    done theme

--- client/src/app/vinyl/page.tsx
+++ client/src/app/vinyl/page.tsx
@@ -3,24 +3,45 @@
 import InfiniteRotate from '@/components/Shared/animations/InfiniteRotate'
 import Stack from '@/components/Vinyl/Stack'
 import Vinyl from '@/components/Vinyl/Vinyl'
+import FadeIn from '@/components/Shared/animations/FadeIn'
 
 const Page = () => {
   return (
     <>
-    <div className='flex flex-col justify-center items-center sm:h-screen mt-20 sm:mt-0'>
-      <div className="w-full flex items-center justify-center">
-        <div className="flex w-full h-full flex-col items-center justify-center sm:flex-row sm:gap-20">
-          <div className='sm:hidden'>Our Memories Playlist</div>
-          <Stack cardDimensions={{ width: 380, height: 440 }} />
-          <div className="relative flex flex-col items-center gap-10 mt-10">
-            <div>Lyrics will be played here</div>
-            <InfiniteRotate>
-              <Vinyl size={{ width: 300, height: 300 }} />
-            </InfiniteRotate>
+      <div className="bg-[url('/vinyl/bgBrown.jpg')] libre-baskerville-regular-italic bg-center bg-fixed p-9 overflow-auto">
+        <div className="w-full flex items-center justify-center">
+          <div className="flex w-full h-full flex-col items-center justify-center sm:flex-row sm:gap-20">
+            <div className="sm:hidden relative">
+              <div className="absolute left-[-125px] top-[30px]">
+                <img src="/vinyl/armleft.png" alt="armleft" className='w-[145px]' />
+              </div>
+              <div className="absolute right-[-125px] top-[-30px]">
+                <img src="/vinyl/armright.png" alt="right" />
+              </div>
+              <FadeIn>
+                <p className="text-[20px] text-[#5B5B5B] libre-baskerville-regular-italic mt-14">Our Memories Playlist</p>
+              </FadeIn>
+            </div>
+            <div className="mt-14 relative">
+              <div className="absolute left-[-30px] top-[300px] z-50">
+                <img src="/vinyl/kid.png" alt="armleft" />
+              </div>
+              <div className="absolute right-[-76px] top-[250px] z-50">
+                <img src="/vinyl/rose.png" alt="right" />
+              </div>
+              <Stack cardDimensions={{ width: 280, height: 340 }} />
+            </div>
+            <div className="relative flex flex-col items-center gap-10 mt-10">
+              <div>Lyrics will be played here</div>
+              <div className="pb-12">
+                <InfiniteRotate>
+                  <Vinyl size={{ width: 250, height: 250 }} />
+                </InfiniteRotate>
+              </div>
+            </div>
           </div>
         </div>
       </div>
-    </div>
     </>
   )
 }
