8721110cd3f3d5d52991d530129fd75f1019eeff
SukumDecha <ninemaster12gt@gmail.com>
2025-02-11 22:31:05 +0700

    :zap: [Update] Complete midnight theme

--- client/src/components/Vinyl/themes/midnight/index.tsx
+++ client/src/components/Vinyl/themes/midnight/index.tsx
@@ -5,18 +5,18 @@ import Stack from "../../Stack"
 import Vinyl from "../../Vinyl"
 import MidNightLayout from "./MidNightLayout"
 
-
-
 const MidNightVinylTheme = () => {
     return (
         <MidNightLayout>
-            <div className="flex flex-col items-center pt-32">
+            <div className="flex flex-col items-center">
                 <div className='sm:hidden text-white text-2xl mb-4'>
                     Our Memories Playlist
                 </div>
 
                 <Stack cardDimensions={{ width: 300, height: 400 }} />
             </div>
+
+
 
             <div className="relative flex flex-col items-center gap-10 mt-10 z-100">
                 <InfiniteRotate>
