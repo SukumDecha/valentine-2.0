771589c4ee8e68e91467794dc7cd78ced6855a03
Melbromss <melbromss@gmail.com>
2025-02-12 14:02:48 +0700

    fixed upload images with texts

--- backend/routes/upload.route.ts
+++ backend/routes/upload.route.ts
@@ -1,9 +1,9 @@
 import {Router} from "express";
-import { uploadFiles } from "../controllers/upload.controller";
+import { uploadFilesWithTexts } from "../controllers/upload.controller";
 import { handleFileUpload } from "../middleware/upload.middleware";
 
 const router = Router();
 
-router.post("/:uuid",handleFileUpload, uploadFiles);
+router.post("/:uuid",handleFileUpload, uploadFilesWithTexts);
 
 export default router;
