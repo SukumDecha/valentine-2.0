bf52e0a15e84e2790ca1365091099b96aaf47229
SukumDecha <ninemaster12gt@gmail.com>
2025-02-13 17:37:21 +0700

    dasdas

--- backend/middleware/upload.middleware.ts
+++ backend/middleware/upload.middleware.ts
@@ -21,7 +21,7 @@ const storage = multer.diskStorage({
 });
 
 const fileFilter = (req: Request, file: Express.Multer.File, cb: multer.FileFilterCallback) => {
-    if (!file.originalname.match(/\.(jpg|jpeg|png|gif|webp)$/i)) {
+    if (!file.originalname.match(/\.(jpg|jpeg|png|gif|webp|HEIC)$/i)) {
         return cb(new Error('Only image files are allowed!'));
     }
     cb(null, true);
