1a1adff7d1f25389c787a5896d9f689d0ed0769d
BoolinBood <napat.jins@mail.kmutt.ac.th>
2025-02-13 11:52:43 +0700

    🎨(styling): add styling to form page

--- client/src/app/form-input/vinyl/page.tsx
+++ client/src/app/form-input/vinyl/page.tsx
@@ -7,9 +7,11 @@ import VinylTemplateForm from './components/VinylTemplateForm'
 const FormInputPage = () => {
   return (
     <>
-      <div className="animate-gradient flex flex-col justify-center relative min-h-screen overflow-hidden bg-gradient-to-r from-[#E990A3] via-pink-500 to-red-500">
-        <div className="p-2 h-full">
+      <div className="animate-gradient relative flex min-h-screen flex-col justify-center overflow-hidden bg-gradient-to-r from-[#E990A3] via-pink-500 to-red-500">
+        <div className="h-full p-2 flex flex-col gap-8">
+          <h1 className='text-2xl font-Libre italic text-white text-center '>Valentine 2.0</h1>
           <VinylTemplateForm />
+          <div className='w-fit self-end rounded-md border border-gray-300 py-1 px-[10px] font-Prompt bg-white bg-opacity-75'>สร้างเว็บไซต์</div>
         </div>
         <style jsx>{`
           @keyframes gradientAnimation {
